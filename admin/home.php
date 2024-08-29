417656d3802c5e8cf838931ed31ed187c22813a8
Muhammed Yakub <yakubmuhammed51@gmail.com>
2024-08-29 17:51:32 +0100

    kast ommit

--- admin/home.php
+++ admin/home.php
@@ -1,10 +1,11 @@
 <h1 class="">Welcome to <?php echo $_settings->info('name') ?> - Admin Side</h1>
 <style>
-  #cover-image{
-    width:calc(100%);
-    height:50vh;
-    object-fit:cover;
-    object-position:center center;
+  #cover-image {
+    width: calc(100%);
+    height: 50vh;
+    object-fit: cover;
+    object-position: center center;
+    backdrop-filter: blur(7px);
   }
 </style>
 <hr>
@@ -16,9 +17,9 @@
       <div class="info-box-content">
         <span class="info-box-text">Total Categories</span>
         <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM category_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM category_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
           <?php ?>
         </span>
@@ -34,9 +35,9 @@
       <div class="info-box-content">
         <span class="info-box-text">Total Shop Type</span>
         <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM shop_type_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM shop_type_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
           <?php ?>
         </span>
@@ -52,9 +53,9 @@
       <div class="info-box-content">
         <span class="info-box-text">Total Products</span>
         <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM product_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM product_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
           <?php ?>
         </span>
@@ -70,9 +71,9 @@
       <div class="info-box-content">
         <span class="info-box-text">Vendors</span>
         <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM vendor_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM vendor_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
           <?php ?>
         </span>
@@ -80,16 +81,17 @@
       <!-- /.info-box-content -->
     </div>
     <!-- /.info-box -->
-  </div><div class="col-12 col-sm-4 col-md-4">
+  </div>
+  <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-maroon elevation-1"><i class="fas fa-user-friends"></i></span>
 
       <div class="info-box-content">
         <span class="info-box-text">Clients</span>
         <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM client_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM client_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
           <?php ?>
         </span>
@@ -105,9 +107,9 @@
       <div class="info-box-content">
         <span class="info-box-text">Total Pending Orders</span>
         <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM order_list where `status` = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM order_list where `status` = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
           <?php ?>
         </span>
@@ -118,8 +120,8 @@
   </div>
 </div>
 
-<div class="clear-fix mb-2">
-    <div class="text-center w-100">
-      <img src="<?= validate_image($_settings->info('cover')) ?>" alt="System Cover image" class="w-100" id="cover-image">
-    </div>
+<div class="clear-fix mb-2 ">
+  <div class="text-center w-100">
+    <img src="<?= validate_image($_settings->info('cover')) ?>" alt="System Cover image" class="w-100" id="cover-image">
   </div>
+</div>
---
+++ database/online-livestock-martket.sql
@@ -0,0 +1,455 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 24, 2024 at 07:15 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online-livestock-martket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_list`
+--
+
+CREATE TABLE `cart_list` (
+  `id` int(30) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `product_id` int(30) NOT NULL,
+  `quantity` float NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_list`
+--
+
+CREATE TABLE `category_list` (
+  `id` int(30) NOT NULL,
+  `vendor_id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category_list`
+--
+
+INSERT INTO `category_list` (`id`, `vendor_id`, `name`, `description`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(1, 1, 'red bororo', 'red borrow', 1, 0, '2024-08-24 17:39:44', NULL),
+(2, 1, 'sokoto Gudali', 'Hazo on X: \"Sokoto Gudali .. a cow with very short horns (more like stumps), it\'s of the Sokoto Gudali', 1, 0, '2024-08-24 17:40:42', NULL),
+(3, 2, 'catfish', 'catfish available', 1, 0, '2024-08-24 17:57:30', NULL),
+(4, 2, 'Titus', 'titusn fish available', 1, 0, '2024-08-24 17:57:47', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_list`
+--
+
+CREATE TABLE `client_list` (
+  `id` int(30) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `firstname` text NOT NULL,
+  `middlename` text DEFAULT NULL,
+  `lastname` text NOT NULL,
+  `gender` text NOT NULL,
+  `contact` text NOT NULL,
+  `address` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `client_list`
+--
+
+INSERT INTO `client_list` (`id`, `code`, `firstname`, `middlename`, `lastname`, `gender`, `contact`, `address`, `email`, `password`, `avatar`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(1, '202408-00001', 'Yakub', '', 'Muhammed', 'Male', '081549372235', 'No 12 Off Old Egume Road, Anyigba', 'muhammedyakub51@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'uploads/clients/1.png?v=1724517916', 1, 0, '2024-08-24 17:45:15', '2024-08-24 17:45:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_id` int(30) NOT NULL,
+  `product_id` int(30) NOT NULL,
+  `quantity` double NOT NULL DEFAULT 0,
+  `price` double NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`order_id`, `product_id`, `quantity`, `price`, `date_created`) VALUES
+(1, 2, 1, 400000, '2024-08-24 17:48:28'),
+(2, 3, 2, 900, '2024-08-24 18:02:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `id` int(30) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `vendor_id` int(30) NOT NULL,
+  `total_amount` double NOT NULL DEFAULT 0,
+  `delivery_address` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_list`
+--
+
+INSERT INTO `order_list` (`id`, `code`, `client_id`, `vendor_id`, `total_amount`, `delivery_address`, `status`, `date_created`, `date_updated`) VALUES
+(1, '202408-00001', 1, 1, 400000, 'No 12 Off Old Egume Road, Anyigba', 5, '2024-08-24 17:48:28', '2024-08-24 17:50:08'),
+(2, '202408-00002', 1, 2, 1800, 'no. 12 off old egume road, anyigba, kogi state', 4, '2024-08-24 18:02:10', '2024-08-24 18:03:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_list`
+--
+
+CREATE TABLE `product_list` (
+  `id` int(30) NOT NULL,
+  `vendor_id` int(30) DEFAULT NULL,
+  `category_id` int(30) DEFAULT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `price` double NOT NULL DEFAULT 0,
+  `image_path` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_list`
+--
+
+INSERT INTO `product_list` (`id`, `vendor_id`, `category_id`, `name`, `description`, `price`, `image_path`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(1, 1, 1, 'red bororo 101', '&lt;p&gt;this coat is very &lt;b&gt;goat&lt;/b&gt; and nice.&lt;/p&gt;', 67500, 'uploads/products/1.png?v=1724517749', 1, 0, '2024-08-24 17:42:28', '2024-08-24 17:42:29'),
+(2, 1, 2, 'sok gudali 102', '&lt;p&gt;this cow is male and has no horn but very big and mighty&lt;/p&gt;', 400000, 'uploads/products/2.png?v=1724517801', 1, 0, '2024-08-24 17:43:21', '2024-08-24 17:43:21'),
+(3, 2, 3, 'catfish001', '&lt;p&gt;&lt;span style=&quot;font-family: Arial, sans-serif; font-size: 14px; background-color: rgb(66, 66, 66);&quot;&gt;&lt;font color=&quot;#000000&quot; style=&quot;&quot;&gt;Catfish are a diverse group of ray-finned fish. Named for their prominent barbels, which resemble a cat&#039;s whiskers, catfish range in size and behavior from the three largest species alive&lt;/font&gt;&lt;/span&gt;&lt;br&gt;&lt;/p&gt;', 900, 'uploads/products/3.png?v=1724518830', 1, 0, '2024-08-24 18:00:30', '2024-08-24 18:00:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop_type_list`
+--
+
+CREATE TABLE `shop_type_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shop_type_list`
+--
+
+INSERT INTO `shop_type_list` (`id`, `name`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(1, 'Poultry Farm', 1, 0, '2024-08-24 17:32:58', NULL),
+(2, 'Fisheries ', 1, 0, '2024-08-24 17:33:25', NULL),
+(3, 'Cattles', 1, 0, '2024-08-24 17:33:50', NULL),
+(4, 'Goats and Sheeps', 1, 0, '2024-08-24 17:34:04', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'ONLINE LIVESTOCK MARKETPLACE'),
+(6, 'short_name', 'PET-LIVESTOCKS'),
+(11, 'logo', 'uploads/logo-1724514507.png'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/cover-1644367404.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-1.png?v=1644472635', NULL, 1, '2021-01-20 14:02:37', '2022-02-10 13:57:15'),
+(11, 'Claire', 'Blake', 'cblake', '4744ddea876b11dcb1d169fadf494418', 'uploads/avatar-11.png?v=1644472553', NULL, 2, '2022-02-10 13:55:52', '2022-02-10 13:55:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendor_list`
+--
+
+CREATE TABLE `vendor_list` (
+  `id` int(30) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `shop_type_id` int(30) NOT NULL,
+  `shop_name` text NOT NULL,
+  `shop_owner` text NOT NULL,
+  `contact` text NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vendor_list`
+--
+
+INSERT INTO `vendor_list` (`id`, `code`, `shop_type_id`, `shop_name`, `shop_owner`, `contact`, `username`, `password`, `avatar`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(1, '202408-00001', 4, 'Isah', 'Okolo seller', '090864227899', 'okolo-seller', '5f4dcc3b5aa765d61d8327deb882cf99', 'uploads/vendors/1.png?v=1724517381', 1, 0, '2024-08-24 17:36:21', '2024-08-24 17:36:21'),
+(2, '202408-00002', 1, 'mr John', 'apka', '081549372235', 'john apka', '5f4dcc3b5aa765d61d8327deb882cf99', 'uploads/vendors/2.png?v=1724518553', 1, 0, '2024-08-24 17:55:52', '2024-08-24 17:55:53');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart_list`
+--
+ALTER TABLE `cart_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `category_list`
+--
+ALTER TABLE `category_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vendor_id` (`vendor_id`);
+
+--
+-- Indexes for table `client_list`
+--
+ALTER TABLE `client_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `vendor_id` (`vendor_id`);
+
+--
+-- Indexes for table `product_list`
+--
+ALTER TABLE `product_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vendor_id` (`vendor_id`),
+  ADD KEY `category_id` (`category_id`) USING BTREE;
+
+--
+-- Indexes for table `shop_type_list`
+--
+ALTER TABLE `shop_type_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vendor_list`
+--
+ALTER TABLE `vendor_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `shop_type_id` (`shop_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart_list`
+--
+ALTER TABLE `cart_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `category_list`
+--
+ALTER TABLE `category_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `client_list`
+--
+ALTER TABLE `client_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_list`
+--
+ALTER TABLE `order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `product_list`
+--
+ALTER TABLE `product_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `shop_type_list`
+--
+ALTER TABLE `shop_type_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `vendor_list`
+--
+ALTER TABLE `vendor_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart_list`
+--
+ALTER TABLE `cart_list`
+  ADD CONSTRAINT `cart_list_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cart_list_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `category_list`
+--
+ALTER TABLE `category_list`
+  ADD CONSTRAINT `category_list_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `vendor_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD CONSTRAINT `order_list_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_list_ibfk_2` FOREIGN KEY (`vendor_id`) REFERENCES `vendor_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `product_list`
+--
+ALTER TABLE `product_list`
+  ADD CONSTRAINT `product_list_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `vendor_list` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `product_list_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `vendor_list`
+--
+ALTER TABLE `vendor_list`
+  ADD CONSTRAINT `vendor_list_ibfk_1` FOREIGN KEY (`shop_type_id`) REFERENCES `shop_type_list` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
